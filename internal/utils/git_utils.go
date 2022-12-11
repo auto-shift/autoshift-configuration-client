23b8daba75953c55278867eee81ba559a4156ea5
Vincent Lewis <vlewis@redhat.com>
2022-12-11 12:05:14 -0500

    added function to clone a git replository

--- internal/utils/git_utils.go
+++ internal/utils/git_utils.go
@@ -2,44 +2,37 @@ package utils
 
 import (
 	"fmt"
+	"os"
 
-	billy "github.com/go-git/go-billy/v5"
 	git "github.com/go-git/go-git/v5"
 	http "github.com/go-git/go-git/v5/plumbing/transport/http"
-	memory "github.com/go-git/go-git/v5/storage/memory"
 )
 
-// git vars
-
-var storer *memory.Storage
-var fs billy.Filesystem
-
 //Methods for interacting with a git repository
+func GitClone(gitUser, gitPass, gitDir, gitUrl string) {
 
-func GitClone(gitUser, gitPass, gitRepo string) {
-	auth := &http.BasicAuth{
-		Username: gitUser,
-		Password: gitPass,
-	}
-
-	r, err := git.Clone(storer, fs, &git.CloneOptions{
-		URL:  gitRepo,
-		Auth: auth,
+	r, err := git.PlainClone(gitDir, false, &git.CloneOptions{
+		// The intended use of a GitHub personal access token is in replace of your password
+		// because access tokens can easily be revoked.
+		// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+		Auth: &http.BasicAuth{
+			Username: gitUser, // yes, this can be anything except an empty string
+			Password: string(gitPass),
+		},
+		URL:      gitUrl,
+		Progress: os.Stdout,
 	})
+	CheckIfError(err)
 
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
+	ref, err := r.Head()
+	CheckIfError(err)
+
+	// ... retrieving the commit object
+	commit, err := r.CommitObject(ref.Hash())
+	if os.IsNotExist(err) {
+		println(err)
 	}
-
-	fmt.Println("Repository cloned")
-
-	w, err := r.Worktree()
-
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
+	fmt.Println(commit)
 
 }
 
@@ -54,4 +47,14 @@ func GitPush(repo, branch string) {
 //TODO: reads git variables from cloned repository
 func GetGitVars() {
 
+}
+
+// CheckIfError should be used to naively panics if an error is not nil.
+func CheckIfError(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	os.Exit(1)
 }
